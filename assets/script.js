d057a9588d601a4e05bf8914b9ed7847608f9e43
Marina Delneri <mgdelneri@gmail.com>
2021-03-04 13:16:34 -0600

    Started HTML structure and wrote out an outline of general steps in JavaScript.

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// First page with instructions and a start button
+
+// Once the start button is pressed, the timer starts and the multiple-choice quiz starts
+
+// When user selects an answer, it automatically brings them to the next question
+    // Whether the answer is right or wrong is displayed before going to the next question
+    // If answer is wrong, some time is subtracted from the clock
+
+// Answers to each questions are stored and then later displayed
+
+// At the end of the quiz OR when the timer runs out, the user is asked to enter their initials 
+    // User presses a button to see the results page
+
+// Results page is shown, with initials of each quiz taker and their score, in descending order
+
