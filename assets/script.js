190de84851ac2fada6ec25dc0c07b440c300732c
Marina Delneri <mgdelneri@gmail.com>
2021-03-04 16:14:21 -0600

    Added text to HTML, styling to CSS, and more detailed pseudo-code in JS>
    clear

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,14 @@
 // First page with instructions and a start button
+    // How to make instruction page disappear when button is pushed
 
 // Once the start button is pressed, the timer starts and the multiple-choice quiz starts
-
-// When user selects an answer, it automatically brings them to the next question
-    // Whether the answer is right or wrong is displayed before going to the next question
-    // If answer is wrong, some time is subtracted from the clock
-
-// Answers to each questions are stored and then later displayed
+    // Function that runs the quiz: function quiz()
+        // Show question and answer options
+        // Make each answer clickable
+        // When user selects an answer, it automatically brings them to the next question
+        // Whether the answer is right or wrong is displayed before going to the next question
+        // If answer is wrong, some time is subtracted from the clock
+        // Answers to each questions are stored and then later displayed as a score
 
 // At the end of the quiz OR when the timer runs out, the user is asked to enter their initials 
     // User presses a button to see the results page
