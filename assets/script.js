72a3fd1fd79ddb590de9e267523f1bc6a298166d
Marina Delneri <mgdelneri@gmail.com>
2021-03-05 14:34:50 -0600

    Styled timer; changed some of the elements of the answers container

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,11 @@ var quizContainer = document.querySelector("#quiz");
 // Create elements for the empty "quizContainer" in HTML
 var timer = document.createElement("p");
 var question = document.createElement("p");
-var answers = document.createElement("ul");
-var option1 = document.createElement("li");
-var option2 = document.createElement("li");
-var option3 = document.createElement("li");
-var option4 = document.createElement("li");
+var answers = document.createElement("div");
+var option1 = document.createElement("div");
+var option2 = document.createElement("div");
+var option3 = document.createElement("div");
+var option4 = document.createElement("div");
 
 // Assign a class to each new element in "quizContainer"
 timer.setAttribute("class", "timer");
@@ -29,14 +29,8 @@ function buildQuiz() {
     // First page with instructions hides when "start the quiz" button is clicked
     instructions.style.visibility = "hidden";
 
-    // Create elements for the empty "quizContainer" in HTML
-    var timer = document.createElement("p");
-    var question = document.createElement("p");
-    var answers = document.createElement("ul");
-    var option1 = document.createElement("li");
-    var option2 = document.createElement("li");
-    var option3 = document.createElement("li");
-    var option4 = document.createElement("li");
+    // Test
+    question.textContent = "QUESTION";
 
     // Append elements to "quizContainer"
     quizContainer.appendChild(timer);
@@ -47,31 +41,27 @@ function buildQuiz() {
     answers.appendChild(option3); 
     answers.appendChild(option4);
 
-    // Assign a class to each new element in "quizContainer"
-    timer.setAttribute("class", "timer");
-    question.setAttribute("class", "question");
-    answers.setAttribute("class", "answers");
-    option1.setAttribute("class", "option");
-    option2.setAttribute("class", "option");
-    option3.setAttribute("class", "option");
-    option4.setAttribute("class", "option");
-
     // Define countdown function
     function countdown() {
         // Start the timer at 60 seconds
-        var timeLeft = 6;
-        // Test
-        question.textContent = "QUESTION";
+        var timeLeft = 15;
+    
         // Define time interval function and attach it to a variable
         var timeInterval = setInterval(function() {
-            if (timeLeft > 1) {
+            if (timeLeft > 10) {
                 timer.textContent = timeLeft + " Seconds Remaining";
+                timeLeft--;
+            } else if (timeLeft < 11 && timeLeft > 1) {
+                timer.textContent = timeLeft + " Seconds Remaining";
+                timer.setAttribute("style", "color: red; font-size: 30px");
                 timeLeft--;
             } else if (timeLeft === 1) {
                 timer.textContent = timeLeft + " Second Remaining";
+                timer.setAttribute("style", "color: red; font-size: 30px");
                 timeLeft--;
             } else {
                 timer.textContent = "Time's Up!!!";
+                timer.setAttribute("style", "color: red; font-size: 30px");
                 clearInterval(timeInterval);
                 // TODO: run function that goes to input name page
             }
@@ -82,6 +72,8 @@ function buildQuiz() {
     }
 
     countdown();
+
+    
 
 
 
